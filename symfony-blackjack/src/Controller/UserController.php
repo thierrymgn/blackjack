fad94ecf3ed2e39ada4736f461005c3433715bdb
deozza <deozza@gmail.com>
2024-02-28 15:39:00 +0100

    added routes for UserController

--- symfony-blackjack/src/Controller/UserController.php
+++ symfony-blackjack/src/Controller/UserController.php
@@ -8,8 +8,71 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    #[Route('/user', name: 'get_user_list', methods: ['GET'])]
+    public function getUserList(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user', name: 'post_user', methods: ['POST'])]
+    public function postUser(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/profile', name: 'get_current_user_infos', methods: ['GET'])]
+    public function getCurrentUserInfos(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/{uuid}', name: 'get_user_infos', methods: ['GET'])]
+    public function getUserInfos(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/profile', name: 'patch_current_user_infos', methods: ['PATCH'])]
+    public function patchCurrentUserInfos(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/{uuid}', name: 'patch_user_infos', methods: ['PATCH'])]
+    public function patchUserInfos(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/profile', name: 'delete_current_user_infos', methods: ['DELETE'])]
+    public function deleteCurrentUserInfos(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+    #[Route('/user/{uuid}', name: 'delete_user_infos', methods: ['DELETE'])]
+    public function deleteUserInfos(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
